5213c1228572c36b0dc41d4efe642813febf0049
rpang808 <rpang16@punahou.edu>
2014-04-20 12:16:35 -1000

    Added Stuff
    added terms of use, poem #1 (contrast of the ocean), and about this
    website

---
+++ About_This_Website_files/About_This_Website.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20140420
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('About_This_Website_files/About_This_WebsiteMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ Terms_of_Use_files/Terms_of_Use.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20140420
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Terms_of_Use_files/Terms_of_UseMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
