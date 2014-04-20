63c7a5b320d0668b3de39e83dc9884fa61c88910
rpang808 <rpang16@punahou.edu>
2014-04-20 12:58:42 -1000

    change to poems title

--- Poems/Poems_files/Poems.js
+++ Poems/Poems_files/Poems.js
@@ -6,6 +6,6 @@ function photocastSubscribe()
 {photocastHelper("http://rpang808.github.io/RTPWaveProject/Poems/rss.xml");}
 function onPageLoad()
 {loadMozillaCSS('Poems_files/PoemsMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixAllIEPNGs('../Media/transparent.gif');fixupAllIEPNGBGs();performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../Media/transparent.gif');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
