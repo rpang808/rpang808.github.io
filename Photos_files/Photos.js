fc6589b8e63b8f549b7d10ce8ada235654b33517
rpang808 <rpang16@punahou.edu>
2014-04-20 11:27:12 -1000

    Build 2
    removed rider names

--- Photos_files/Photos.js
+++ Photos_files/Photos.js
@@ -1,12 +1,12 @@
 // Created by iWeb 3.0.4 local-build-20140420
 
 function createMediaStream_id2()
-{return IWCreatePhotocast("http://RTPWaveProject/Photos_files/rss.xml",true);}
+{return IWCreatePhotocast("http://rpang808.github.io/RTPWaveProject/Photos_files/rss.xml",true);}
 function initializeMediaStream_id2()
-{createMediaStream_id2().load('http://RTPWaveProject',function(imageStream)
+{createMediaStream_id2().load('http://rpang808.github.io/RTPWaveProject',function(imageStream)
 {var entryCount=imageStream.length;var headerView=widgets['widget1'];headerView.setPreferenceForKey(imageStream.length,'entryCount');NotificationCenter.postNotification(new IWNotification('SetPage','id2',{pageIndex:0}));});}
 function layoutMediaGrid_id2(range)
-{createMediaStream_id2().load('http://RTPWaveProject',function(imageStream)
+{createMediaStream_id2().load('http://rpang808.github.io/RTPWaveProject',function(imageStream)
 {if(range==null)
 {range=new IWRange(0,imageStream.length);}
 IWLayoutPhotoGrid('id2',new IWPhotoGridLayout(3,new IWSize(191,191),new IWSize(191,30),new IWSize(229,236),27,27,0,new IWSize(5,5)),new IWPhotoFrame([IWCreateImage('Photos_files/comic-1_01.png'),IWCreateImage('Photos_files/comic-1_02.png'),IWCreateImage('Photos_files/comic-1_03.png'),IWCreateImage('Photos_files/comic-1_06.png'),IWCreateImage('Photos_files/comic-1_09.png'),IWCreateImage('Photos_files/comic-1_08.png'),IWCreateImage('Photos_files/comic-1_07.png'),IWCreateImage('Photos_files/comic-1_04.png')],null,0,0.714286,4.000000,4.000000,1.000000,1.000000,5.000000,5.000000,6.000000,6.000000,71.000000,115.000000,71.000000,115.000000,null,null,null,0.500000),imageStream,range,null,null,1.000000,{backgroundColor:'rgb(0, 0, 0)',reflectionHeight:100,reflectionOffset:2,captionHeight:100,fullScreen:0,transitionIndex:2},'Media/slideshow.html','widget1','widget2','widget3')});}
