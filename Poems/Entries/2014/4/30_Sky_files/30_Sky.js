dd2f16b1bf6e8536b1970e06238b150eaada53c7
rpang808 <rpang16@punahou.edu>
2014-05-16 08:19:17 -1000

    good stuff

---
+++ Poems/Entries/2014/4/30_Sky_files/30_Sky.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20140516
+
+setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('30_Sky_files/30_SkyMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');BlogFixupPreviousNext();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
--- Welcome_files/Welcome.js
+++ Welcome_files/Welcome.js
@@ -1,9 +1,11 @@
-// Created by iWeb 3.0.4 local-build-20140425
+// Created by iWeb 3.0.4 local-build-20140516
 
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000}),stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,295),url:'Welcome_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'Welcome_files/stroke_1.png'},{rect:new IWRect(1,-1,221,2),url:'Welcome_files/stroke_2.png'},{rect:new IWRect(222,-1,2,2),url:'Welcome_files/stroke_3.png'},{rect:new IWRect(222,1,2,295),url:'Welcome_files/stroke_4.png'},{rect:new IWRect(222,296,2,3),url:'Welcome_files/stroke_5.png'},{rect:new IWRect(1,296,221,3),url:'Welcome_files/stroke_6.png'},{rect:new IWRect(-1,296,2,3),url:'Welcome_files/stroke_7.png'}],new IWSize(223,297))});registry.applyEffects();}
+function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Welcome_files/WelcomeMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
